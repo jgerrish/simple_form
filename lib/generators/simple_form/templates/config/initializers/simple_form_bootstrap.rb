ec7b162a7adfdb94b3a2a298dd79de27014fd096
Joshua Gerrish <jgerrish@gmail.com>
2013-10-28 11:44:24 -0700

    Update simple_form_bootstrap.rb config initializer to Bootstrap 3

--- lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
@@ -1,9 +1,10 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, tag: 'div', class: 'control-group', error_class: 'error' do |b|
+  config.wrappers :bootstrap, tag: 'div', class: 'form-group', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
+    b.use :input, class: 'form-control'
     b.wrapper tag: 'div', class: 'controls' do |ba|
       ba.use :input
       ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
@@ -42,4 +43,30 @@ SimpleForm.setup do |config|
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
   config.default_wrapper = :bootstrap
+
+  # config/initializers/remove-this-sf_bs3_inputs.rb
+  inputs = %w[
+  CollectionSelectInput
+  DateTimeInput
+  FileInput
+  GroupedCollectionSelectInput
+  NumericInput
+  PasswordInput
+  RangeInput
+  StringInput
+  TextInput
+  ]
+
+  inputs.each do |input_type|
+    superclass = "SimpleForm::Inputs::#{input_type}".constantize
+
+    new_class = Class.new(superclass) do
+      def input_html_classes
+        super.push('form-control')
+      end
+    end
+
+    Object.const_set(input_type, new_class)
+  end
+
 end
